8c8160dc81e89ea9af649f64632d7490fd91ad2c
Esteban Cuesta <cuestaesteban@yahoo.com>
2019-04-30 16:09:07 -0400

    added overdraft protection

--- app/main.js
+++ app/main.js
@@ -4,15 +4,26 @@ function depositMoney() {
   let currentBal = bal.innerText
   let total = parseInt(currentBal) + parseInt(amount)
   bal.innerText = total
+  history.innerHTML += "<p>You deposited" + amount + "</p>"
 }
 
 function withdrawMoney() {
   let amount = prompt("How much do you want to withdraw?")
+  let history = document.querySelector("#history")
+  let OD = document.querySelector("#odProtect")
   let bal = document.querySelector("#bank-balance")
   let currentBal = bal.innerText
   let total = parseInt(currentBal) - parseInt(amount)
+  if (OD.checked() == true) {
+    bal.innerText = bal
+    alert("Overdraft Prevented")
+  }
+  if (total <= 0) {
+    bal.classList.add("negative-balance")
+  }
   bal.innerText = total
   if (total <= 0) {
     bal.classList.add("negative-balance")
   }
+  history.innerHTML += "<p>You withdrew" + amount + "</p>"
 }
