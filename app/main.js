95c23e34efa00aecb3164e4b65f503e63890d2ed
Esteban Cuesta <cuestaesteban@yahoo.com>
2019-04-30 15:24:18 -0400

    added animations to app

--- app/main.js
+++ app/main.js
@@ -1,3 +1,18 @@
 function depositMoney() {
   let amount = prompt("How much do you want to deposit?")
+  let bal = document.querySelector("#bank-balance")
+  let currentBal = bal.innerText
+  let total = parseInt(currentBal) + parseInt(amount)
+  bal.innerText = total
+}
+
+function withdrawMoney() {
+  let amount = prompt("How much do you want to withdraw?")
+  let bal = document.querySelector("#bank-balance")
+  let currentBal = bal.innerText
+  let total = parseInt(currentBal) - parseInt(amount)
+  bal.innerText = total
+  if (total <= 0) {
+    bal.classList.add("negative-balance")
+  }
 }
